c102e4d87691612d187686c35eaeba3825b39d5f
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-02 20:17:39 -0400

    Add `ChunkHeader`

---
+++ Sources/ShockwaveFile/ChunkHeader.swift
@@ -0,0 +1,18 @@
+import BinaryParsing
+
+/// The 8-byte tag + length header that precedes every RIFX chunk's payload.
+public struct ChunkHeader: Sendable {
+  public var tag: FourCharCode
+  /// The payload length in bytes, not including this header.
+  public var length: Int
+
+  public init(tag: FourCharCode, length: Int) {
+    self.tag = tag
+    self.length = length
+  }
+
+  public init(parsing input: inout ParserSpan, byteOrder: Endianness) throws(ParsingError) {
+    tag = try FourCharCode(parsing: &input)
+    length = try Int(parsing: &input, storedAs: UInt32.self, endianness: byteOrder)
+  }
+}
