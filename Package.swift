4f9c5ec4049a4af40c9611a21594ad9bf8c739c8
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-02 21:43:31 -0400

    Add `ShockwaveModel` target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
         .macOS(.v14)
     ],
     products: [
-        .library(name: "ShockwaveFile", targets: ["ShockwaveFile"])
+        .library(name: "ShockwaveFile", targets: ["ShockwaveFile"]),
+        .library(name: "ShockwaveModel", targets: ["ShockwaveModel"])
     ],
     dependencies: [
         .package(url: "https://github.com/MillerTechnologyPeru/swift-lingo", from: "0.2.1"),
@@ -25,6 +26,25 @@ let package = Package(
         .testTarget(
             name: "ShockwaveFileTests",
             dependencies: ["ShockwaveFile"],
+            resources: [.copy("Resources")],
+            swiftSettings: [.enableUpcomingFeature("ApproachableConcurrency")]
+        ),
+        .target(
+            name: "ShockwaveModel",
+            dependencies: [
+                "ShockwaveFile",
+                .product(name: "LingoRuntime", package: "swift-lingo"),
+                .product(name: "LingoBytecode", package: "swift-lingo")
+            ],
+            swiftSettings: [.enableUpcomingFeature("ApproachableConcurrency")]
+        ),
+        .testTarget(
+            name: "ShockwaveModelTests",
+            dependencies: [
+                "ShockwaveModel", "ShockwaveFile",
+                .product(name: "LingoRuntime", package: "swift-lingo"),
+                .product(name: "LingoBytecode", package: "swift-lingo")
+            ],
             resources: [.copy("Resources")],
             swiftSettings: [.enableUpcomingFeature("ApproachableConcurrency")]
         )
